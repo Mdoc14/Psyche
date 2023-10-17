1206e0b021af84a87edf2cf02e2eb93f25db9d8e
carlossantayana <96289335+carlossantayana@users.noreply.github.com>
2023-10-18 00:50:45 +0200

    OptionsMenu and Intro
    Se implementó el menú de opciones en la pantalla de título y la cinemática inicial.
    
    Ya está creada la escena principal del juego.

---
+++ Assets/Scripts/Menus/SettingsMenu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetMasterVolume(float volume) //Función que recibe un float para modificar el canal Master del audioMixer con ese valor.
+    {
+        audioMixer.SetFloat("volumeMaster", volume); //Le asigna el valor del parámetro volume al parámetro volumeMaster del audioMixer.
+    }
+
+    //Las siguientes dos funciones funcionan de forma equivalente.
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("volumeMusic", volume);
+    }
+
+    public void SetSoundEffectsVolume(float volume)
+    {
+        audioMixer.SetFloat("volumeSoundEffects", volume);
+    }
+}
