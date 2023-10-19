0823d570f53628f20539a316c02e74b3a74635a0
carlossantayana <96289335+carlossantayana@users.noreply.github.com>
2023-10-19 19:38:14 +0200

    Main Menu Changes
    Se ajustaron algunas funciones del menú principal y se borró la SampleScene.

--- Assets/Scripts/Menus/MainMenuBehaviour.cs
+++ Assets/Scripts/Menus/MainMenuBehaviour.cs
@@ -5,20 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuBehaviour : MonoBehaviour
 {
-    public GameObject panel;
     public void Play() 
     {
         SceneManager.LoadScene("Intro");
     }
 
-    public void OpenOptions() 
-    { 
-        panel.SetActive(true);
-    }
-    public void CloseOptions() 
-    {
-        panel.SetActive(false);
-    }
     public void Quit() 
     {
         Debug.Log("Saliendo del juego ...");
