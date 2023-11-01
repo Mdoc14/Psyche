976320d0278ced94329e7d2d05edd49f4850c84c
Sebastian02L <sebaslf02@gmail.com>
2023-11-01 15:24:57 +0100

    Funcionalidad de Visiblidad V2.1
    Funcionalidad completamente implementada.
    Nuevo mapa integrado en el mapa, cambio en los scripts de visibilidad, todo el piso 1 funciona.
    
    Se comentó el código del script "Observer_VisibilityControl" y "Subject_ActualRoom".
    Se borró el código de la carpeta de pruebas.
    Se borró la escena de pruebas.
    Se creó un "SkyBox" (Un cubo negro debajo del mapa) de color negro para hacer de fondo.

--- Assets/Scripts/Observer_VisibilityControl.cs
+++ Assets/Scripts/Observer_VisibilityControl.cs
@@ -7,11 +7,12 @@ public class Observer_VisiilityControl : MonoBehaviour, IObserver<float>
 {
     //Atributos de la clase
 
+    //Nos serviran de punteros a las salas actuales donde este el jugador
     private List<GameObject> salaActualSinParedes;
     private List<GameObject> salaActualConMuros;
 
 
-    //Listas con las paredes que se desactivaran en cada sala
+    //Listas con las paredes y objetos que se desactivaran en cada sala para permitir una buena vision de la misma
     public List<GameObject> habitacionParedes = new List<GameObject>();
     public List<GameObject> pasilloParedes = new List<GameObject>();
     public List<GameObject> servicioParedes = new List<GameObject>();
@@ -21,7 +22,7 @@ public class Observer_VisiilityControl : MonoBehaviour, IObserver<float>
     public List<GameObject> comedorParedes = new List<GameObject>();
     public List<GameObject> salonParedes = new List<GameObject>();
 
-    //Listas con los muros que apareceran en la base del suelo
+    //Listas con los muros que apareceran en la base del suelo de la sala donde este el jugador, permitiendo una buena vision
     public List<GameObject> habitacionMuros = new List<GameObject>();
     public List<GameObject> pasilloMuros = new List<GameObject>();
     public List<GameObject> servicioMuros = new List<GameObject>();
@@ -31,7 +32,7 @@ public class Observer_VisiilityControl : MonoBehaviour, IObserver<float>
     public List<GameObject> comedorMuros = new List<GameObject>();
     public List<GameObject> salonMuros = new List<GameObject>();
 
-    //Listas con todos los objetos de cada piso, incluyendo suelos y paredes
+    //Listas con todos los objetos de cada piso, incluyendo suelos y paredes, esto para hacer los cambios de un piso a otro
     public List<GameObject> piso1GameObjects = new List<GameObject>();
     public List<GameObject> piso0GameObjects = new List<GameObject>();
 
@@ -40,15 +41,23 @@ public class Observer_VisiilityControl : MonoBehaviour, IObserver<float>
         //Buscamos al jugador y suscribimos esta clase a su sujeto
         GameObject.FindWithTag("Player").GetComponent<Subject_ActualRoom>().AddObserver(this);
 
-        quitRoom(habitacionParedes);
-        chargeRoom(habitacionMuros);
+        //Ocultamos la planta baja para que esta no se vea desde arriba
+        visibilityOff(piso0GameObjects);
+
+        //Ocultamos las paredes de la habitacion donde aparece el jugador
+        visibilityOff(habitacionParedes);
+
+        //Mostramos los muros que ocupan el lugar de las paredes ocultas
+        visibilityOn(habitacionMuros);
+
+        //Apuntamos a estas listas con los "punteros"
         salaActualSinParedes = habitacionParedes;
         salaActualConMuros = habitacionMuros;
 
     }
 
-    //Recorre la lista y pone en false el atributo "enabled" del MeshRenderer. Ocultando los GameObject que no queremos que se vean 
-    public void quitRoom(List<GameObject> lista)
+    //Recorre la lista y pone en false el atributo "enabled" del MeshRenderer. Ocultando los GameObject/Paredes que no queremos que se vean 
+    public void visibilityOff(List<GameObject> lista)
     {
         foreach (GameObject obj in lista)
         {
@@ -56,8 +65,8 @@ public class Observer_VisiilityControl : MonoBehaviour, IObserver<float>
         }
     }
 
-    //Recorre la lista y pone en true el atributo "enabled" del MeshRenderer. Mostrando los GameObject que queremos que se vean
-    public void chargeRoom(List<GameObject> lista)
+    //Recorre la lista y pone en true el atributo "enabled" del MeshRenderer. Mostrando los GameObject/Paredes que queremos que se vean
+    public void visibilityOn(List<GameObject> lista)
     {
         foreach (GameObject obj in lista)
         {
@@ -65,17 +74,27 @@ public class Observer_VisiilityControl : MonoBehaviour, IObserver<float>
         }
     }
     
+    //Realiza los cambios de visibilidad necesarias de las habitaciones cuando es llamada
     public void updateEnviroment(List<GameObject> salaSinParedes, List<GameObject> salaConMuros, List<GameObject> nextSalaSinParedes, List<GameObject> nextSalaConMuros)
     {
-        chargeRoom(salaSinParedes);
-        quitRoom(salaConMuros);
+        //Muestra las paredes/GameObjects ocultadas de la sala donde estaba el jugador
+        visibilityOn(salaSinParedes);
 
+        //Oculta los muros de la sala donde estaba el jugador
+        visibilityOff(salaConMuros);
+
+        //Actualiza los "punteros"
         salaActualSinParedes = nextSalaSinParedes;
         salaActualConMuros = nextSalaConMuros;
 
-        quitRoom(nextSalaSinParedes);
-        chargeRoom(nextSalaConMuros);
+        //Oculta las paredes/GameObjects visibles de la sala donde esta el jugador
+        visibilityOff(nextSalaSinParedes);
+
+        //Muestra los muros de la sala donde esta el jugador
+        visibilityOn(nextSalaConMuros);
     }
+
+    //Llamado desde el metodo Notify() del sujeto, en funcion del numero recibido hara la llamada a updateEnviroment con los parametros necesarios
     public void UpdateObserver(float room)
     {
         switch (room)
@@ -119,19 +138,45 @@ public class Observer_VisiilityControl : MonoBehaviour, IObserver<float>
 
                 updateEnviroment(salaActualSinParedes, salaActualConMuros, salonParedes, salonMuros);
                 break;
+
             case 9: //Cambio de piso, piso actual: planta baja
 
-                quitRoom(piso1GameObjects);
-                quitRoom(salaActualConMuros);
+                //Ocultamos el piso donde se encontraba el jugador
+                visibilityOff(piso1GameObjects);
 
+                //Mostramos los GameObjects / Paredes del piso al que va el jugador
+                visibilityOn(piso0GameObjects);
+
+                //Mostramos los muros de la sala en la que estara el jugador en el nuevo piso
+                visibilityOff(salaActualConMuros);
+
+                //Actualizamos los "punteros"
                 salaActualSinParedes = pasilloP0Paredes;
                 salaActualConMuros = pasilloP0Muros;
 
-                chargeRoom(salaActualSinParedes);
-                chargeRoom(salaActualConMuros);
+                //Ocultamos los GameObject/Paredes de la sala actual y mostramos los muros del mismo
+                visibilityOff(salaActualSinParedes);
+                visibilityOn(salaActualConMuros);
                 break;
 
             case 10: //Cambio de piso, piso actual: piso 1
+
+                //Ocultamos el piso donde se encontraba el jugador
+                visibilityOff(piso0GameObjects);
+
+                //Mostramos los GameObjects / Paredes del piso al que va el jugador
+                visibilityOn(piso1GameObjects);
+
+                //Mostramos los muros de la sala en la que estara el jugador en el nuevo piso
+                visibilityOff(salaActualConMuros);
+
+                //Actualizamos los "punteros"
+                salaActualSinParedes = pasilloParedes;
+                salaActualConMuros = pasilloMuros;
+
+                //Ocultamos los GameObject/Paredes de la sala actual y mostramos los muros del mismo
+                visibilityOff(salaActualSinParedes);
+                visibilityOn(salaActualConMuros);
                 break;
 
             default:
--- Assets/Scripts/Subject_ActualRoom.cs
+++ Assets/Scripts/Subject_ActualRoom.cs
@@ -10,7 +10,7 @@ public class Subject_ActualRoom : MonoBehaviour, ISubject<float>
     //Lista con los observadores del sujeto
     public List<IObserver<float>> observadores = new List<IObserver<float>>();
 
-    //Entero que indicará el numero de sala
+    //Entero que indicara el numero de sala
     float room = 1;
     
     //Suscribe al observador recibido a este sujeto
@@ -19,7 +19,7 @@ public class Subject_ActualRoom : MonoBehaviour, ISubject<float>
         observadores.Add(observer);
     }
 
-    //Notifica a todos los observadores de la lista
+    //Notifica a todos los observadores de la lista, le pasa el valor de room a UpdateObserver()
     public void NotifyObservers()
     {
         foreach (IObserver<float> obs in observadores) 
@@ -34,6 +34,7 @@ public class Subject_ActualRoom : MonoBehaviour, ISubject<float>
         observadores.Remove(observer);
     }
 
+    //Al colisionar, en funcion del tag del trigger, cambiara el valor de room y hara una llamada al metodo Notify()
     private void OnTriggerEnter(Collider other)
     {
 
@@ -80,6 +81,11 @@ public class Subject_ActualRoom : MonoBehaviour, ISubject<float>
         if (other.CompareTag("PlantaBaja"))
         {
             room = 9;
+        }
+
+        if (other.CompareTag("Piso1"))
+        {
+            room = 10;
         }
 
         NotifyObservers();
