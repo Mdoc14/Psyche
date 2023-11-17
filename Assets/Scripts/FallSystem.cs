6dc9fccfd6e40d1099c1186aa843aef085f63735
Sebastián Loges <sebaslf02@gmail.com>
2023-11-17 14:00:52 +0100

    Prototipo de Sistema AntiCaida
    Se creó un script y se agrego un game object vacio a la escena

---
+++ Assets/Scripts/FallSystem.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+//Componente que hace que el jugador vuelva al escenario si se cae
+public class FallSystem : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 playerSpawn;
+
+    private void Awake()
+    {
+        //Encontramos la posicion
+        playerSpawn = GameObject.FindWithTag("Player").transform.position;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            //Si la escena es la de la Casa
+            if(SceneManager.GetActiveScene() == SceneManager.GetSceneByBuildIndex(2)){
+                other.transform.position = playerSpawn;
+            }
+
+        }
+    }
+}
