ab0ea8b3209c716774a3b0bec2ce0f7db7a7a751
Mdoc14 <marcos.deozaeta@gmail.com>
2024-01-11 21:53:00 +0100

    Cambio de nombre del singleton

--- Assets/Scripts/Patterns/Singleton/Components/PersistentGlobalVolume.cs
+++ Assets/Scripts/Patterns/Singleton/Components/PersistentGlobalVolume.cs
@@ -2,6 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PersistentGameObject : Singleton<PersistentGameObject>
+public class PersistentGlobalVolume : Singleton<PersistentGlobalVolume>
 {
 }
